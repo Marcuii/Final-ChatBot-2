f9ebe92d03028a37fe2385a4702bc01411468fe7
Nouraessam123 <138504609+Nouraessam123@users.noreply.github.com>
2025-05-12 01:50:05 +0300

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from dotenv import load_dotenv
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-model_name = "gpt-3.5-turbo"
+model_name = "gpt-4"
 analyzer = SentimentIntensityAnalyzer()
 
 app = Flask(__name__)
